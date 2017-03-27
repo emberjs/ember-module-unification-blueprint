98d13b13eb4f9ce1f166f0d654ae9e04bc1c0468
Matthew Beale <matt.beale@madhatted.com>
2017-03-26 23:27:44 -0700

    Use glimmer-wrapper

--- files/src/resolver.js
+++ files/src/resolver.js
@@ -1,4 +1,4 @@
-import Resolver from 'ember-resolver/resolvers/module-unification';
+import Resolver from 'ember-resolver/resolvers/glimmer-wrapper';
 import buildResolverConfig from 'ember-resolver/ember-config';
 import config from '../config/environment';
 

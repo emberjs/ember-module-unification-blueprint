25e530f1a16cf2d0f87585ba575120720e5cc11b
Matthew Beale <matt.beale@madhatted.com>
2017-07-08 13:52:53 -0700

    Add point to extend moduleConfig

--- files/src/resolver.js
+++ files/src/resolver.js
@@ -2,6 +2,12 @@ import Resolver from 'ember-resolver/resolvers/glimmer-wrapper';
 import buildResolverConfig from 'ember-resolver/ember-config';
 import config from '../config/environment';
 
+let moduleConfig = buildResolverConfig(config.modulePrefix);
+/*
+ * If your application has custom types and collections, modify moduleConfig here
+ * to add support for them.
+ */
+
 export default Resolver.extend({
-  config: buildResolverConfig(config.modulePrefix)
+  config: moduleConfig
 });
